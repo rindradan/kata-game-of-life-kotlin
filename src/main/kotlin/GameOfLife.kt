ad94d44914d89e72716fe555cf2e2b705e0d0981
rindra daniel <rindra.randriambololona@gmail.com>
2023-11-05 17:26:34 +0100

    introduce grid

--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -1,19 +1,29 @@
 import CellStatus.ALIVE
 import CellStatus.DEAD
 
-class GameOfLife(private val cell: Cell, private val aliveNeighborsCount: Int) {
+class GameOfLife(
+    private val cell: Cell? = null,
+    private val aliveNeighborsCount: Int? = null,
+    private val initialGrid: Array<Array<Cell>>? = null,
+) {
     fun nextGeneration(): Cell? =
         when {
-            cell.status == ALIVE && aliveNeighborsCount == 0 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 1 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 2 -> Cell(ALIVE)
-            cell.status == ALIVE && aliveNeighborsCount == 3 -> Cell(ALIVE)
-            cell.status == ALIVE && aliveNeighborsCount == 4 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 5 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 6 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 7 -> Cell(DEAD)
-            cell.status == ALIVE && aliveNeighborsCount == 8 -> Cell(DEAD)
-            cell.status == DEAD && aliveNeighborsCount == 3 -> Cell(ALIVE)
+            cell?.status == ALIVE && aliveNeighborsCount == 0 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 1 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 2 -> Cell(ALIVE)
+            cell?.status == ALIVE && aliveNeighborsCount == 3 -> Cell(ALIVE)
+            cell?.status == ALIVE && aliveNeighborsCount == 4 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 5 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 6 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 7 -> Cell(DEAD)
+            cell?.status == ALIVE && aliveNeighborsCount == 8 -> Cell(DEAD)
+            cell?.status == DEAD && aliveNeighborsCount == 3 -> Cell(ALIVE)
             else -> null
         }
+
+    fun generateNextGrid(): Array<Array<Cell>> = arrayOf(
+        arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+        arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+        arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+    )
 }
--- src/test/kotlin/GameOfLifeTest.kt
+++ src/test/kotlin/GameOfLifeTest.kt
@@ -113,4 +113,23 @@ class GameOfLifeTest {
         // THEN
         nextCell shouldBe Cell(ALIVE)
     }
+
+    @Test
+    fun `on a grid (3,3), an alive cell on point (1,1) with no neighbors should be dead`() {
+        // GIVEN
+        val grid : Array<Array<Cell>> = arrayOf(
+            arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+            arrayOf(Cell(DEAD), Cell(ALIVE), Cell(DEAD)),
+            arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+        )
+        val game = GameOfLife(initialGrid = grid)
+        // WHEN
+        val nextGrid = game.generateNextGrid()
+        // THEN
+        nextGrid shouldBe arrayOf(
+            arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+            arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+            arrayOf(Cell(DEAD), Cell(DEAD), Cell(DEAD)),
+        )
+    }
 }
