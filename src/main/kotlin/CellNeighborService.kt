b3b46ab9d7efe79dcc249ca99f6a4f1e012755b9
rindra daniel <rindra.randriambololona@gmail.com>
2023-11-06 00:06:29 +0100

    remove irrelevant tests

--- src/main/kotlin/CellNeighborService.kt
+++ src/main/kotlin/CellNeighborService.kt
@@ -4,11 +4,6 @@ class CellNeighborService {
     fun getAliveNeighborsCount(grid: Array<Array<Cell>>, targetCellRowIndex: Int, targetCellColumnIndex: Int): Int =
         getNeighbors(grid, targetCellRowIndex, targetCellColumnIndex).filter { it == Cell(ALIVE) }.size
 
-    fun getNeighborsCount(grid: Array<Array<Cell>>, cellRowIndex: Int, cellColumnIndex: Int): Int =
-        if (cellRowIndex in grid.indices && cellColumnIndex in grid[cellRowIndex].indices) {
-            getNeighbors(grid = grid, cellRowIndex = cellRowIndex, cellColumnIndex = cellColumnIndex).size
-        } else 0
-
     private fun getNeighbors(grid: Array<Array<Cell>>, cellRowIndex: Int, cellColumnIndex: Int): Array<Cell> = arrayOf(
         getNeighbor(grid, cellRowIndex + (-1), cellColumnIndex + (-1)),
         getNeighbor(grid, cellRowIndex + (-1), cellColumnIndex + 0),
